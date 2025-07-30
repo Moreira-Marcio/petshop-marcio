1effbdabf7cf62d1db7ab7aca30bc7e78940022f
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-07-30 11:42:07 -0300

    adiciona link dinamico com link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pagina não encontrada |PetShop",
+  robots: "noimdex",
+};
+
 export default function Pagina404() {
   return (
     <section>
