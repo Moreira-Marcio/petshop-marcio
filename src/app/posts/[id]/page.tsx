4ad00a590898a8f29a638d8971b8d6a66a9f68f6
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-08-04 11:36:50 -0300

    separa post por categoria

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -9,6 +9,25 @@ type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
+export async function generateMetadata({ params }: DetalhePostProps) {
+  const { id } = await params;
+
+  const resultado = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resultado.ok) {
+    throw new Error("Erro ao buscar os posts: " + resultado.statusText);
+  }
+
+  const post: Post = await resultado.json();
+
+  return {
+    title: post.titulo + "| PetShop",
+    description: post.descricao,
+  };
+}
+
 export default async function DetalhePost({ params }: DetalhePostProps) {
   const { id } = await params;
 
