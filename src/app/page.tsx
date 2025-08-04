6c70ed64cdf59d33aa40cddaa8ba8e92b639bc29
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-08-04 09:16:10 -0300

    atualiza fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,19 @@
 import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 import arrayPosts from "@/data/array-posts";
+import { Post } from "@/types/Post";
 
-export default function Home() {
+export default async function Home() {
+  const resposta = await fetch(`http://localhost2112/posts`, {
+    next: { revalidate: 0 },
+  });
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar os posts: " + resposta.statusText);
+  }
+
+  const posts: Post = await resposta.json();
+  console.log(posts);
+
   return (
     <section className={estilos.conteudo}>
       <h2>Pet Notícias</h2>
