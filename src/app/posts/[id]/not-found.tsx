bda4e72efeaf1acb75e2bdc669076b35230f790a
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-08-08 11:21:19 -0300

    configura not foud para rota de posts

---
+++ src/app/posts/[id]/not-found.tsx
@@ -0,0 +1,15 @@
+//src/app/posts/[id]/not-found.tsx
+import Container from "@/components/Container";
+import estilos from "./not-found.module.css";
+import Link from "next/link";
+
+export default function PostNaoEncontrado() {
+    return(
+        <section className={estilos.conteudo}>
+           <h2>Ops!! Post não encontrado</h2>
+           <Container>
+            <p>O post que você esta tentando acessar não existe.</p>
+            <p><Link  href="/">Voltar para pagina principal</Link></p>
+           </Container>
+        </section>
+    )}
