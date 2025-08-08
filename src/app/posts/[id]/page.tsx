a49c36827205042094fe9fcc956025043e86b612
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-08-08 11:44:37 -0300

    configura page not-found

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,6 +3,7 @@ import estilos from "./detalhe-post.module.css";
 
 import { Post } from "@/types/Post";
 import next from "next";
+import { notFound } from "next/navigation";
 
 // src/app/posts/[id]/page.tsx
 type DetalhePostProps = {
@@ -15,6 +16,11 @@ export async function buscarPostPorId(id: string): Promise<Post> {
   const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
     next: { revalidate: 0 },
   });
+
+  if (resposta.status === 404) {
+    //buscar page not-found.tsx automaticamente em caso de erro 404
+    notFound();
+  }
 
   if (!resposta.ok) {
     throw new Error("Erro ao buscar o post: " + resposta.statusText);
