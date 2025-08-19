73f2bb8fcf1964fb444ec47f96f5ffeaa2e84367
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-08-19 10:51:08 -0300

    configura supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,14 @@
+//src/lib/supabase.ts
+
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("as variaveis de ambiente devem ser definidas");
+}
+
+//exportando uma const supabase com os recursos de acesso do back-end
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
